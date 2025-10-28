08036802abc3b354b5133a246cfecc8554507c58
Aarav <iamcoder18@gmail.com>
2025-10-28 16:11:25 -0600

    fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,60 +1,14 @@
 import type React from "react"
 import type { Metadata } from "next"
-import localFont from 'next/font/local'
+import { Geist } from 'next/font/google'
 import { Analytics } from "@vercel/analytics/next"
 import { Suspense } from "react"
 import { Toaster } from "sonner"
 import "./globals.css"
 
-const geistSans = localFont({
-  src: [
-    {
-      path: '../node_modules/geist/dist/fonts/geist-sans/Geist-Thin.woff2',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: '../node_modules/geist/dist/fonts/geist-sans/Geist-UltraLight.woff2',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: '../node_modules/geist/dist/fonts/geist-sans/Geist-Light.woff2',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../node_modules/geist/dist/fonts/geist-sans/Geist-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../node_modules/geist/dist/fonts/geist-sans/Geist-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../node_modules/geist/dist/fonts/geist-sans/Geist-SemiBold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../node_modules/geist/dist/fonts/geist-sans/Geist-Bold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../node_modules/geist/dist/fonts/geist-sans/Geist-Black.woff2',
-      weight: '800',
-      style: 'normal',
-    },
-    {
-      path: '../node_modules/geist/dist/fonts/geist-sans/Geist-UltraBlack.woff2',
-      weight: '900',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-geist-sans',
+const geistSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist',
   display: 'swap',
 })
 
